2e4116358c81b8669a3e22b6fabc3605fa79f667
davekch <dave-koch@web.de>
2020-04-29 14:13:55 +0200

    add command line options to server

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ mod player;
 mod tichugame;
 mod tichuserver;
 
+#[macro_use]
+extern crate clap;
+use clap::App;
 use log::error;
 use simplelog::{Config, LevelFilter, TermLogger, TerminalMode};
 use tichuserver::TichuServer;
@@ -11,7 +14,13 @@ use tichuserver::TichuServer;
 fn main() {
     // set up logger
     let _ = TermLogger::init(LevelFilter::Debug, Config::default(), TerminalMode::Mixed);
-    let server = TichuServer::accept("127.0.0.1", "1001");
+    // set up argument parser
+    let options = load_yaml!("cli.yml");
+    let args = App::from_yaml(options).get_matches();
+    let ip = args.value_of("ip_address").unwrap_or("127.0.0.1");
+    let port = args.value_of("port").unwrap_or("1001");
+
+    let server = TichuServer::accept(ip, port);
     match server {
         Ok(mut server) => {
             server.main();
